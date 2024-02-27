2fd5dbbcde8884ba760393e1e8dc1d9ff49c4547
ADITYA  PAWAR <64777945+adip47@users.noreply.github.com>
2024-02-27 14:49:04 +0530

    Create main.tf

---
+++ dev/main.tf
@@ -0,0 +1,29 @@
+terraform {
+   backend "local" {
+       path     = "../../state/terraform.tfstate"
+}
+}
+ 
+provider "aws" {
+    region = "us-east-1"
+}
+ 
+terraform {
+   required_providers {
+   aws = "~>2.34"
+}
+}
+ 
+variable "path"  { default = "../state/terraform.tfstate" }
+variable "region" {
+    default = "us-east-1"
+}
+ 
+variable "env" {
+  default = "dev"
+}
+ 
+module "s3" {
+  source = "" [please put s3 repo url here ]
+  env_name = "dev"
+}
