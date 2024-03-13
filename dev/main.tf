670e36d468133ab6d891b59f6e813823c031c9dd
ADITYA  PAWAR <64777945+adip47@users.noreply.github.com>
2024-03-13 18:51:14 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -35,3 +35,48 @@ module "sqs" {
   source = "../../AWSforTerraform/SQS" 
   env_name = "dev"
 }
+terraform {
+   backend "local" {
+       path     = "../../state/terraform.tfstate"
+}
+}
+ 
+provider "aws" {
+    region = "us-east-1"
+}
+ 
+terraform {
+   required_providers {
+   aws = "~>2.34"
+}
+}
+ 
+variable "path"  { default = "../state/terraform.tfstate" }
+variable "region" {
+    default = "us-east-1"
+}
+ 
+variable "env" {
+  default = "dev"
+}
+ 
+/*module "s3" {
+  source = "../../AWSforTerraform/S3"
+  env_name = "dev"
+}
+module "sns" {
+  source = "../../AWSforTerraform/SNS"
+  env_name = "dev"
+}
+module "sqs" {
+  source = "../../AWSforTerraform/SQS"
+  env_name = "dev"
+}*/
+ 
+module "vpc2" {
+source = "../../AWSforTerraform/vpc2"
+env_name = var.env_name
+public_subnet_cidrs = var.public_subnet_cidrs
+private_subnet_cidrs = var.private_subnet_cidrs
+azs = var.azs
+}
